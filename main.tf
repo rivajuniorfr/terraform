e7d1d747edb66b99032d4ade43fe248129bf56ec
rivajuniorfr <rivajunior.rec@gmail.com>
2022-02-09 10:35:25 -0300

    Criado o Bucket dev4 e mais uma instancia dev5

--- main.tf
+++ main.tf
@@ -22,7 +22,27 @@ resource "aws_instance" "dev" {
     }
     vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
 }
+resource "aws_instance" "dev4" {
+  ami = "ami-026c8acd92718196b"
+  instance_type = "t2.micro"
+  key_name = "terraform-aws"
+  tags = {
+    Name = "dev4"
+  }
+  vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
+  depends_on = ["aws_s3_bucket.dev4"]
+}
 
+resource "aws_instance" "dev5" {
+  ami = "ami-026c8acd92718196b"
+  instance_type = "t2.micro"
+  key_name = "terraform-aws"
+  tags = {
+    Name = "dev5"
+  }
+   vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
+}
+ 
 resource "aws_security_group" "acesso-ssh" {
   name        = "acesso-ssh"
   description = "acesso-ssh"
@@ -37,6 +57,14 @@ resource "aws_security_group" "acesso-ssh" {
   }
   tags = {
     Name = "ssh"
+  }
+   }
+   resource "aws_s3_bucket" "dev4" {
+  bucket = "rivas-dev4"
+  acl    = "private"
+
+  tags = {
+    Name = "rivas-dev4"
   }
 }
 
